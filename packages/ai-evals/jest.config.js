16b52baa160f2d2086807f0527f8b028c43898ad
drgaciw <drgamtd@gmail.com>
2026-01-31 08:51:42 -0600

    fix(ai-evals): set module/moduleResolution in jest tsconfig for enum support
    Added module and resolution settings to tsconfig in Jest config.

--- packages/ai-evals/jest.config.js
+++ packages/ai-evals/jest.config.js
@@ -1,32 +1,34 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    roots: ['<rootDir>/src'],
-    testMatch: ['**/__tests__/**/*.test.ts'],
-    collectCoverageFrom: [
-          'src/**/*.ts',
-          '!src/**/*.d.ts',
-          '!src/**/__tests__/**',
-        ],
-    coverageThreshold: {
-          global: {
-                  branches: 80,
-                  functions: 80,
-                  lines: 80,
-                  statements: 80,
-          },
-    },
-    moduleNameMapper: {
-          '^@aah/(.*)$': '<rootDir>/../$1/src',
-    },
-    transform: {
-          '^.+\\.tsx?$': [
-                  'ts-jest',
-            {
-                      tsconfig: {
-                                  esModuleInterop: true,
-                      },
-            },
-                ],
-    },
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      roots: ['<rootDir>/src'],
+      testMatch: ['**/__tests__/**/*.test.ts'],
+      collectCoverageFrom: [
+              'src/**/*.ts',
+              '!src/**/*.d.ts',
+              '!src/**/__tests__/**',
+            ],
+      coverageThreshold: {
+              global: {
+                        branches: 80,
+                        functions: 80,
+                        lines: 80,
+                        statements: 80,
+              },
+      },
+      moduleNameMapper: {
+              '^@aah/(.*)$': '<rootDir>/../$1/src',
+      },
+      transform: {
+              '^.+\\.tsx?$': [
+                        'ts-jest',
+                  {
+                              tsconfig: {
+                                            esModuleInterop: true,
+                                            module: 'commonjs',
+                                            moduleResolution: 'node',
+                              },
+                  },
+                      ],
+      },
 };
